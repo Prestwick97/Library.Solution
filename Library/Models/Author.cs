ef2915f5e4f9a62863789d697a782d985a1cc942
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-24 12:04:39 -0700

    create Author class
    co-authored by Patrick Delaney <Prestwick97
    /
    co-authored by Patrick Delaney <prestwick97@gmail.com>
    co-authored by Fatma Dogan <fceyda@gmail.com>

--- Library/Models/Author.cs
+++ Library/Models/Author.cs
@@ -8,6 +8,7 @@ namespace Library.Models
     {
       this.Books = new HashSet<AuthorBook>();
     }
+    
     public int AuthorId {get; set;}
     public string Name {get; set;}
     public virtual ICollection<AuthorBook> Books {get; set;}
