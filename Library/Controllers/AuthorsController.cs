8dba2c63cd5191a6683d74100ec3fef223d7a0d2
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-24 13:33:47 -0700

    create AuthorsController.cs
    co-authored by Patrick Delaney <prestwick97@gmail.com>
    co-authored by Fatma Dogan <fceyda@gmail.com>

---
+++ Library/Controllers/AuthorsController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Controllers
+{
+  public class AuthorsController : Controller
+  {
+    private readonly LibraryContext _db;
+
+    public AuthorsController(LibraryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Author> model = _db.Authors.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Author author)
+    {
+      _db.Authors.Add(author);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisCategory = _db.Categories
+        .Include(category => category.Items)
+        .ThenInclude(join => join.Item)
+        .FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+  }
+}
