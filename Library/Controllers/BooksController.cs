78db02c213b190294eacbc8d2cdff4dc53b01195
Prestwick97 <prestwick97@gmail.com>
2020-03-25 15:45:18 -0700

    further modify bookscontroller
    Co-authored-by: Fatma Dogan <fceyda@gmail.com>
    > Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -50,10 +50,10 @@ namespace Library.Controllers
           _db.AuthorBook.Add(new AuthorBook() { AuthorId = AuthorId, BookId = book.BookId, CopiesId = CopiesId});
         }
       
-      // var thisBook = _db.Books
-      //   .Include(book => book.Title)
-      //   .ThenInclude(join => join.Copies)
-      //   .FirstOrDefault(book => copies.Count +=1);
+      var thisBook = _db.Books
+        .Include(book => book.Title)
+        .ThenInclude(join => join.Copies)
+        .FirstOrDefault(book => copies.Count +=1);
 
       _db.SaveChanges();
       return RedirectToAction("Index");
